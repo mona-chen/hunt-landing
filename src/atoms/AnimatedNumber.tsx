a107b77a2bb2fc4d6b730139e83ddedb758ba47c
Ezeani Emmanuel <65641745+mona-chen@users.noreply.github.com>
2025-06-16 13:27:23 +0100

    Install dependencies with Bun

--- src/atoms/AnimatedNumber.tsx
+++ src/atoms/AnimatedNumber.tsx
@@ -1,19 +1,7 @@
 import NumberFlow from "@number-flow/react";
-import { useEffect, useState } from "react";
 
-export default function AnimatedNumber({value}: {value:string | number}) {
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    alert("Component mounted");
-    // return () => console.log("Component unmounted");
-  }, []);
-
-  const [number, setNumber] = useState(0)
-
-  useEffect(() => {
-    // setNumber(value as number)
-  }, [value])
+export default function AnimatedNumber({value}: {value: string | number}) {
   return (
+    <NumberFlow value={value} />
   );
 }
